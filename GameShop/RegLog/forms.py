a0812b46ba14459db91b2dd031ab1a252bc9d1b5
Вершинин Николай <113011702+Faster190@users.noreply.github.com>
2024-12-05 17:54:45 +0300

    Delete GameShop directory

--- GameShop/RegLog/forms.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-
-from RegLog.models import Buyer
-
-
-class RegisterForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = Buyer
-        fields = UserCreationForm.Meta.fields + ("email",)
--- GameShop/RegLog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', home),
-    path('profile/', profile_view, name='profile'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('logout/', logout_user, name='logout_user'),
-]
