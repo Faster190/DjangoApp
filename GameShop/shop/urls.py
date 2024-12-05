f7804c222665257be768fb9844579d41e2a9c499
Вершинин Николай <113011702+Faster190@users.noreply.github.com>
2024-12-05 17:52:48 +0300

    Add files via upload

--- GameShop/shop/urls.py
+++ GameShop/shop/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # path('about/', about, name="about"),
     path('', main_page, name="main"),
     path("game/<slug:game_slug>/", game_page),
     path("about/", page_about, name="about"),
     path("<slug:game_genre>/", genre_page),
-    # path('<int:category>/', main_page_category),
 ]
--- GameShop/shop/views.py
+++ GameShop/shop/views.py
@@ -2,11 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import *
 
+
 # Create your views here.
 
 def main_page(request, head="Жанры"):
     head_genre = head
-    games = []
     if head == "Жанры":
         games = Games.objects.all().order_by('-id')
     else:
@@ -15,10 +15,6 @@ def main_page(request, head="Жанры"):
         head_genre = genre_.name
     result = []
     for game in games:
-        list_ = []
-        # for gen in game.genre.all():
-        #     list_.append(gen)
-        # print(list_)
         dict_ = {
             'title': game.title,
             'genre': game.genre.all(),
@@ -34,6 +30,7 @@ def main_page(request, head="Жанры"):
     }
     return render(request, "shop/main.html", context)
 
+
 def game_page(request, game_slug):
     game = get_object_or_404(Games, slug=game_slug)
     genres = game.genre.all()
@@ -44,6 +41,7 @@ def game_page(request, game_slug):
         "head": "Жанры",
     }
     return render(request, "shop/game_page.html", context)
+
 
 def genre_page(request, game_genre):
     return main_page(request, head=game_genre)
